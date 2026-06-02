964cb705fcaf85871fbabe78c528da2fa6313a1d
chch20040916-cloud <chch20040916@gmail.com>
2026-06-02 08:22:16 -1200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("IdeaMart Backend Running");
+});
+
+app.post("/api/notify", (req, res) => {
+  console.log(req.body);
+
+  res.status(200).send("Notification Received");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
